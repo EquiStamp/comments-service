954e386854b78238d2ec3e9dfbba5390b94f0513
Daniel O'Connell <github@ahiru.pl>
2024-08-12 19:41:49 +0200

    remove .env file

--- src/comments/settings.py
+++ src/comments/settings.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DB_USERNAME = os.environ.get("DB_USERNAME", "comments_admin")
-DB_PASSWORD = os.environ.get("DB_PASSWORD", "dont let me down")
+DB_PASSWORD = os.environ.get("DB_PASSWORD", "let it be, let it be")
 DB_ENDPOINT = os.environ.get("DB_ENDPOINT", "0.0.0.0")
 DB_PORT = os.environ.get("DB_PORT", 3306)
 DB_NAME = os.environ.get("DB_NAME", "comments_db")
